789ca502f442e59e50d7ce9cd9c910a8999ad4e2
Antonio de Luna <tonydelun@gmail.com>
2016-02-18 22:08:50 -0800

    added a test function to make code cleaner

--- main.cpp
+++ main.cpp
@@ -3,6 +3,34 @@
 #include <cassert>
 #include <cstring>
 
+void test_exception(const char* operation, const char* exception);
+void test_answer(const char* operation, long long exception);
+
+int main() {
+  test_answer("5 5 + 3", 8);
+  test_answer("5 + 3" , 8);
+  test_answer("5 + 3 + 6" , 14);
+  test_answer("5 - 3", 2);
+  test_answer("5 - 3 - 1", 1);
+  test_answer("5 + 3 - 1", 7);
+  test_answer("5 * 3", 15);
+  test_answer("5 * 3 * 2", 30);
+  test_answer("5 - 3 * 2", -1);
+  test_answer("4 / 2", 2);
+  test_answer("3 / 2", 1);
+  test_answer("4 + 4 / 2", 6);
+
+  test_exception("+", "Too few operands");
+  test_exception("5 + + 3", "Too few operands");
+  test_exception("1000000000000", "Integer Overflow");
+  test_exception("999999999999 + 1", "Integer Overflow");
+  test_exception("999999999999 * 2", "Integer Overflow");
+}
+void test_answer(const char* operation, long long result) {
+  assert(parse(operation) == result);
+  std::cout<<"assertion passed '"<< operation <<"' = " << result<<std::endl;
+}
+
 void test_exception (const char* operation, const char* exception) {
   const char * test = "";
   try {
@@ -14,34 +42,4 @@ void test_exception (const char* operation, const char* exception) {
   assert(strcmp(test, exception) == 0);
   std::cout<<"assertion passed '"<< operation <<"' = " << exception <<std::endl;
 
-}
-
-int main() {
-  assert(parse("5 + 3") == 8); std::cout<<"assertion passed '5 + 3' = 8"<<std::endl;
-  assert(parse("5 + 3 + 6") == 14);
-  std::cout<<"assertion passed '5 + 3 + 6' = 14"<<std::endl;
-  assert(parse("5 - 3") == 2);
-  std::cout<<"assertion passed '5 - 3' = 2"<<std::endl;
-  assert(parse("5 - 3 - 1") == 1);
-  std::cout<<"assertion passed '5 - 3 - 1' = 1"<<std::endl;
-  assert(parse("5 + 3 - 1") == 7);
-  std::cout<<"assertion passed '5 + 3 - 1' = 7"<<std::endl;
-  assert(parse("5 * 3") == 15);
-  std::cout<<"assertion passed '5 * 3' = 15"<<std::endl;
-  assert(parse("5 * 3 * 2") == 30);
-  std::cout<<"assertion passed '5 * 3 * 2' = 30"<<std::endl;
-  assert(parse("5 - 3 * 2") == -1);
-  std::cout<<"assertion passed '5 - 3 * 2' = -1"<<std::endl;
-  assert(parse("4 / 2") == 2);
-  std::cout<<"assertion passed '4 / 2' = 2"<<std::endl;
-  assert(parse("3 / 2") == 1);
-  std::cout<<"assertion passed '3 / 2' = 1"<<std::endl;
-  assert(parse("4 + 4 / 2") == 6);
-  std::cout<<"assertion passed '4 + 4 / 2' = 6"<<std::endl;
-
-  test_exception("+", "Too few operands");
-  test_exception("5 + + 3", "Too few operands");
-  test_exception("1000000000000", "Integer Overflow");
-  test_exception("999999999999 + 1", "Integer Overflow");
-  test_exception("999999999999 * 2", "Integer Overflow");
 }
